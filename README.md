1782f0fdb6f17ada0e7a84413a6345179c8b89fc
Alexander.Shvedov <shvedov89@gmail.com>
2020-02-02 10:55:20 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vplotter
 V-plotter powered by Raspberry Pi / .NET Core / C#
+
+http://blog.oddbit.com/post/2017-09-26-some-notes-on-pwm-on-the-raspberry-pi/
---
+++ vplotter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Device.Gpio;
+
+namespace VPlotterCore
+{
+  public static class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Hello, .NET on PI!");
+
+      return;
+
+      using var controller = new GpioController(PinNumberingScheme.Board);
+
+
+      const int kotiPin = 15;
+
+      using var pwmChannel = System.Device.Pwm.PwmChannel.Create(chip: 0, channel:0, frequency: 50);
+      pwmChannel.Start();
+
+      while (true)
+      {
+        Console.Write("value=");
+        var line = Console.ReadLine();
+        if (!int.TryParse(line, out var value)) continue;
+
+        if (value < 0 || value > 100) continue;
+
+        pwmChannel.DutyCycle = (double) value / 100;
+      }
+
+      //controller.OpenPin(kotiPin, PinMode.Output);
+
+      // for (var index = 0;; index++)
+      // {
+      //   var pinValue = index % 2 == 0 ? PinValue.High : PinValue.Low;
+      //   Console.WriteLine(pinValue);
+      //   controller.Write(kotiPin, pinValue);
+      //   Thread.Sleep(TimeSpan.FromSeconds(1));
+      // }
+    }
+  }
+}
